fa98c1b5c58c4509b78957b7e0673dcab37bf465
VishalSoni2005 <vsoni0882@gmail.com>
2025-07-03 20:17:52 +0530

    last of today

--- DSA/03_Recursion/11_Permutation1.cpp
+++ DSA/03_Recursion/11_Permutation1.cpp
@@ -1,10 +1,43 @@
-#include<iostream>
+//! Time complexty : O(n * n!);
+//! space complexty: O(n);
+
+#include <bits/stdc++.h>
 using namespace std;
+
+void permutation(vector<int> &nums, vector<vector<int>> &ans, vector<int>& temp, vector<bool>& visited)
+{
+  if (temp.size() == nums.size()) {
+    ans.push_back(temp);
+    return;
+  }
+
+  for (int i = 0; i < nums.size(); i++) // Try every element from current index onward
+  {
+    if (visited[i] == 0) 
+    {
+      visited[i] = 1;
+      temp.push_back(nums[i]);
+      permutation(nums, ans, temp, visited);
+      visited[i] = 0;
+      temp.pop_back();
+    }
+  }
+}
 
 int main()
 {
-    int arr[] = {1, 2, 3};
-    int n = sizeof arr/ sizeof arr[0];
-    
-    return 0;
+  vector<int> nums = {1, 2, 3};
+  vector<vector<int>> ans;
+
+  vector<bool> visited = {0, 0, 0};
+  vector<int> helper;
+  permutation(nums, ans, helper, visited);
+
+  cout << "All possible permutations are n! : " << endl;
+  for (auto i : ans)
+  {
+    for (int j : i)
+      cout << j << " ";
+    cout << endl;
+  }
 }
