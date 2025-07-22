d63637ed39bf9a960df9e557bf5464732875a5c1
VishalSoni2005 <vsoni0882@gmail.com>
2025-07-22 22:07:58 +0530

    Refactor rob function to utilize TAB method for optimal performance

--- DSA/15_**DP**/01_House_Robber_1.cpp
+++ DSA/15_**DP**/01_House_Robber_1.cpp
@@ -1,38 +1,69 @@
 class Solution {
  public:
-  // int find(vector<int>& nums, int size, int idx, vector<int>&dp) {
+  int rec_solve(vector<int>& nums, int size, int idx) {
+    if (idx >= size)
+      return 0;
 
-  //     if (idx >= size)
-  //         return 0;
+    return max(nums[idx] + rec_solve(nums, size, idx + 2),
+               rec_solve(nums, size, idx + 1));
+  }
+  int TD(vector<int>& nums, int size, int idx, vector<int>& dp) {
+    if (idx >= size)
+      return 0;
 
-  //     if(dp[idx] != -1) return dp[idx];
+    if (dp[idx] != -1)
+      return dp[idx];
 
-  //     return dp[idx] =  max(nums[idx] + find(nums, size, idx + 2, dp),
-  //                find(nums, size, idx + 1, dp));
-  // }
+    return dp[idx] = max(nums[idx] + TD(nums, size, idx + 2, dp),
+                         TD(nums, size, idx + 1, dp));
+  }
 
-  // int rob(vector<int>& nums) {
-  //     int n = nums.size();
-  //     vector<int>dp(n+2, -1);
-  //     dp[n] = 0, dp[n + 1] = 0;
-  //     // return find(nums, n, 0, dp);
-
-  //     for(int i = n-1; i > -1; i--)
-  //       dp[i] = max(dp[i+2] + nums[i], dp[i+1]);
-  //     return dp[0];
-  // }
-
-  int rob(vector<int>& nums) {
+  int BU(vector<int>& nums) {
     int n = nums.size();
+    vector<int> dp(n + 2, -1);
+
+    dp[n] = dp[n + 1] = 0;
+
+    for (int i = n - 1; i > -1; i--) {
+      int last = dp[i + 2];
+      int prev = dp[i + 1];
+      int curr = nums[i];
+      dp[i] = max(last + curr, prev);
+    }
+
+    return dp[0];
+  }
+
+  int TAB(vector<int>& nums) {
+    int n = nums.size();
+
     int ans;
     int first = 0;
     int second = 0;
 
     for (int i = n - 1; i > -1; i--) {
-      ans = max(nums[i] + second, first);
+      int curr = nums[i];
+
+      ans = max(curr + second, first);
       second = first;
       first = ans;
     }
+
     return ans;
+  }
+
+  int rob(vector<int>& nums) {
+    int n = nums.size();
+    if (n == 1)
+      return nums[0];
+
+    // return max(rec_solve(nums, n, 0), rec_solve(nums, n, 1));
+
+    // vector<int>dp(n+2, -1);
+    // return max(TD(nums, n, 0, dp), TD(nums, n, 1, dp));
+
+    // return BU(nums);
+
+    return TAB(nums);
   }
 };
