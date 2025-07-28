56b2ca3cc2eac26c7671a7484e0a7cd0c32ebb30
VishalSoni2005 <vsoni0882@gmail.com>
2025-07-28 22:42:34 +0530

    DP on string variants

---
+++ DSA/15_**DP**/DP_ON_String/02_min_del_ins.cpp
@@ -0,0 +1,25 @@
+class Solution {
+ public:
+  int minOperations(string &s1, string &s2) {
+    // Your code goes here
+    int m = s1.length();
+    int n = s2.length();
+
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        if (s1[i - 1] == s2[j - 1])
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+
+    int lcsLen = dp[m][n];
+    int deletions = m - lcsLen;
+    int insertions = n - lcsLen;
+
+    return deletions + insertions;
+  }
+};
