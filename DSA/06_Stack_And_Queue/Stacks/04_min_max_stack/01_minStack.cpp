cae5da86e41765f6c521a05d45868658de129d2c
VishalSoni2005 <vsoni0882@gmail.com>
2025-09-12 21:09:43 +0530

    Started Next Pattern min/max stack

---
+++ DSA/06_Stack_And_Queue/Stacks/04_min_max_stack/01_minStack.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+  stack<int> st, ms;
+
+ public:
+  MinStack() {}
+
+  void push(int val) {
+    st.push(val);
+
+    if (ms.empty() or val <= ms.top()) {
+      ms.push(val);
+    }
+  }
+
+  void pop() {
+    if (st.empty())
+      return;
+    if (st.top() == ms.top())
+      ms.pop();
+
+    st.pop();
+  }
+
+  int top() { return st.top(); }
+
+  int getMin() { return ms.top(); }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
