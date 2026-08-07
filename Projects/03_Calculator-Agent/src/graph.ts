8d32bcb1f4e8036dd0b33a5213618aea2f856723
vishalsoni2005 <vsoni0882@gmail.com>
2026-08-07 12:53:55 +0530

    feat(Projects): renamed folders for convinience

--- Projects/03_Calculator-Agent/src/graph.ts
+++ Projects/03_Calculator-Agent/src/graph.ts
@@ -1,26 +1,17 @@
 import { StateGraph, START, END } from "@langchain/langgraph";
+import { toolsCondition } from "@langchain/langgraph/prebuilt";
 
-import { ToolNode, toolsCondition } from "@langchain/langgraph/prebuilt";
-import { chatbot } from "./nodes/chatbot.node";
 import { CalculatorState } from "./states/state";
+
+import { chatbot } from "./nodes/chatbot.node";
 import { toolNode } from "./nodes/tool-node.node";
 
 const graph = new StateGraph(CalculatorState)
-
   .addNode("chatbot", chatbot)
-
   .addNode("tools", toolNode)
-
   .addEdge(START, "chatbot")
-
-  .addConditionalEdges(
-    "chatbot",
-
-    toolsCondition,
-  )
-
+  .addConditionalEdges("chatbot", toolsCondition)
   .addEdge("tools", "chatbot")
-
   .addEdge("chatbot", END);
 
 export const app = graph.compile();
--- Projects/03_Calculator-Agent/src/index.ts
+++ Projects/03_Calculator-Agent/src/index.ts
@@ -1,16 +1,24 @@
 import "dotenv/config";
-
 import { HumanMessage } from "@langchain/core/messages";
-
 import { app } from "./graph";
 
+const msg = [
+  {
+    role: "system",
+    content:
+      "You are a seductress and have to answer every question in a seductive way. Do not reveal the answer directly, instead tease the user with it. Only give the final answer at the end.",
+  },
+  { role: "user", content: "What is (25 + 7) * 3?" },
+];
 
 async function main() {
   const result = await app.invoke({
-    messages: [new HumanMessage("What is (25 + 7) * 3?")],
+    messages: msg,
   });
 
   console.log(result.messages.at(-1)?.content);
+
+  console.log("Printing metadata result obj: ", result.messages[0].response_metadata);
 }
 
 main();
