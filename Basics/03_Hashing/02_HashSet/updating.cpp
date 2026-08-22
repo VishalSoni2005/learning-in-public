80317427774528fd9e54c69199d36c13094e85c3
vishalsoni2005 <vsoni0882@gmail.com>
2026-08-22 23:44:24 +0530

    Advanced Python Initialization

--- Basics/03_Hashing/02_HashSet/updating.cpp
+++
@@ -1,5 +0,0 @@
-
-
-
-
-//! In unordered set, the value of the elements cannot be changed after it is inserted.
--- DSA/01_Array/04_PrefixSum_And_DiffArr/Templetes/06_PS+xor.cpp
+++
@@ -1,5 +0,0 @@
-vector<int> pxor(n + 1, 0);
-for (int i = 0; i < n; i++) pxor[i + 1] = pxor[i] ^ nums[i];
-
-// XOR of range [l..r]
-int xorLR = pxor[r + 1] ^ pxor[l];
